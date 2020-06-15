533207332d6fa8b470c3bb91661ed9596b9902d5
alpharez92 <66931269+alpharez92@users.noreply.github.com>
2020-06-15 10:48:54 +0700

    Add files via upload
    tugas 3

---
+++ tugas 3.php
@@ -0,0 +1,8 @@
+<?php
+    echo' Nama : Rizki Alfa Reza <br>';
+    echo' Kelas : XI Rpl 1 <br>';
+    echo "SMK Taruna Bhakti";
+    echo "Jatijajar RT08 RW09";
+    echo' Email : rizkireza63@gmail.com<br>' ;
+    echo' No Telepon : 0895402180101<br>';
+    ?>
